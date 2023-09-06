b00716f11ca027df1b1c40b4ee31a1d085f37416
OneGraund_macbook <vovangaunder@gmail.com>
2023-09-06 15:49:34 +0200

    Changed location

--- run_bot.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-
-REM Starting bot.py pyton script with all bots...
-cd C:\ucs\UCSNotificationCenter
-git pull origin main
-pip install -r requirements.txt
-start "" /B python bot.py updated
