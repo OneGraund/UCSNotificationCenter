21dd092a816694861e24c4c9e901f388f235216e
OneGraund_macbook <vovangaunder@gmail.com>
2023-09-06 22:23:17 +0200

    added polling check try and except for testing

--- bot.py
+++ bot.py
@@ -384,7 +384,11 @@ class TelegramChanel:
 
         time.sleep(2)
         if TEST:
-            self.bot.polling()
+            try:
+                self.bot.polling()
+                print('Polling good...')
+            except:
+                print(f'[{utils.get_date_and_time()}] [{self.str_name}] Polling bad... ‼️ TELEGRAM API ISSUE ‼️')
         else:
             self.bot.infinity_polling()
 
